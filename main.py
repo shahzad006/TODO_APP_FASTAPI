69517fd4cdfdbd081aa90e33618578315fa7a923
Muhammad Shahzad <muhammadshahzad45435@gmail.com>
2026-07-24 11:33:23 +0500

    Get Single Todo by ID

--- main.py
+++ main.py
@@ -23,3 +23,13 @@ def create_todo(todo:Todo):
 @app.get("/todos")
 def get_todos():
     return todos
+
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {
+        "error" : "Todo not found"
+    }
